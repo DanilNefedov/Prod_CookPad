9bac20bd70d9a595039ed1b6b13d9a7711de14c5
DanilNefedov <ndanil2@ukr.net>
2025-09-01 14:23:57 +0300

    deploy error

--- app/components/home/content/CardContentBlock.tsx
+++ app/components/home/content/CardContentBlock.tsx
@@ -2,7 +2,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Box, CardHeader, Collapse, Menu, MenuItem, useMediaQuery } from "@mui/material";
+import { Box, Collapse, Menu, MenuItem, useMediaQuery } from "@mui/material";
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import Link from "next/link";
